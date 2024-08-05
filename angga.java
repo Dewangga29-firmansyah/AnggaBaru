d5eb34e5ad8930ef5cd2583516fa26c736c919e3
Dewangga29-firmansyah <dewangga_firmansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:11:02 +0700

    Add files via upload

---
+++ angga.java
@@ -0,0 +1,18 @@
+    public class angga {
+    public static void main(String[] args) {
+        String nama="Dewangga";
+        String alamat="Tulungagung";
+        int usia =16;
+        char jenisKelamin='L';
+        double beratBadan=56.00;
+      
+        System.out.println("Nama:"+ nama);
+        System.out.println("Alamat:"+ alamat);
+        System.out.println("Usia:"+ usia);
+        System.out.println("jeniskelamin:"+ jenisKelamin);
+        System.out.println("BeratBadan:"+ beratBadan );
+        }
+    }
+    
+        
+    
